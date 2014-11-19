029afbefa645af45e1b101d8fb3a482b8d590451
igalna <igalna@gmail.com>
2014-11-19 20:01:18 +0000

    java docs for FunctionalLinkedList.java and an update to the rest() method to return a FunctionalList with the elements in the same order as the calling FunctionalList

--- FunctionalLinkedList.java
+++ FunctionalLinkedList.java
@@ -1,16 +1,38 @@
+/**
+*
+*   A FunctionalLinkedList is an extension of LinkedList which implements the FunctionalList interface
+*
+*/
+
 public class FunctionalLinkedList extends LinkedList implements FunctionalList {
 
+    /**
+    *
+    *   head() method creates a deep copy of the element at the head of the LinkedList
+    *
+    *   @return a deep copied ReturnObject with the same value as the ReturnObject at the head of the LinkedList
+    *
+    */
+    @Override
     public ReturnObject head() {
         ReturnObjectImpl result = new ReturnObjectImpl(super.get(0).getReturnValue());
         return result;
     }
     
+    /**
+    *
+    *   rest() method creates a new FunctionalList from the elements of the calling FunctionalLinkedList
+    *   then removes the head element
+    *   
+    *   @return a deep copy of the calling LinkedList with the head element removed
+    *
+    */
     public FunctionalList rest() {
-        FunctionalLinkedList result = new FunctionalLinkedList();
-        
-        
-        for (int i = 0; i < this.size(); i++) {
-            result.add(get(i).getReturnValue());
+        FunctionalList result = new FunctionalLinkedList();
+        int count = this.size()-1;
+        for (int i = 0; i < this.size()-1; i++) {
+            result.add(new ReturnObjectImpl(this.get(count).getReturnValue()));
+            count--;
         }
         result.remove(0);
         return result;
